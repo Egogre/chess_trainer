2c0b56c22b562a3087d07db678cf1f6e3b9b54c3
philmphoenix <jpholzmann@gmail.com>
2015-12-08 14:09:45 -0700

    Calls in a board and makes sure we can instantiate.

--- test/board-test.js
+++ test/board-test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+var Board = require('../lib/board')
+
 describe('Board', function () {
   it('should instantiate a new board', function () {
     let board = new Board();
