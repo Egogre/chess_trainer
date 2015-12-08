39cdd51d008edb59ffdfdeaed66b18194cfd17ca
philmphoenix <jpholzmann@gmail.com>
2015-12-08 14:56:05 -0700

    White space

--- test/board-test.js
+++ test/board-test.js
@@ -5,7 +5,7 @@ var Board = require('../lib/board')
 
 describe('Board', function () {
   it('should instantiate a new board', function () {
-    let board = new Board();
+    let board = new Board ();
     assert.isObject(board);
   });
 });
