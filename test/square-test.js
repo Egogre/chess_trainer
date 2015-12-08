69b472a05e9af03fdb66e2ebe90de778aae55e9d
philmphoenix <jpholzmann@gmail.com>
2015-12-08 14:32:13 -0700

    Adds tests for coordinates, existence and a reference to board.

---
+++ test/square-test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+var Board = require('../lib/board')
+var Square = require('../lib/square')
+
+describe('Square', function () {
+  beforeEach( function () {
+    this.board = new Board ();
+  });
+
+  it('should instantiate a new square', function () {
+    let square = new Square ();
+    assert.isObject(square);
+  });
+
+  it('should know its coordinates', function () {
+    let square = new Square (this.board, 3, 5);
+    assert.equal(square.xCoordinate, 3);
+    assert.equal(square.yCoordinate, 5);
+  });
+
+  it('should know the board it belongs to', function () {
+    let square = new Square (this.board, 3, 5);
+    assert.equal(square.board, this.board);
+  })
+});
