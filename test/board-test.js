5c4637d3183f82b847e03c66f37a155ec1f9c4a6
philmphoenix <jpholzmann@gmail.com>
2015-12-09 15:46:06 -0700

    Adds tests to board for canvas, context and game over.

--- test/board-test.js
+++ test/board-test.js
@@ -8,4 +8,19 @@ describe('Board', function () {
     let board = new Board ();
     assert.isObject(board);
   });
+
+  it('should know the canvas it is built on', function () {
+    let board = new Board ('context', 'canvas');
+    assert.equal(board.canvas, 'canvas')
+  })
+
+  it('should know the context it is built in', function () {
+    let board = new Board ('context', 'canvas');
+    assert.equal(board.context, 'context')
+  })
+
+  it('should know when the game is over', function () {
+    let board = new Board ();
+    assert.equal(board.gameOver, false)
+  });
 });
