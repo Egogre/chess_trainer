575e502cdef8e485cf2f0a1a92f8686ece21f6df
George Hudson <georgehudson78@gmail.com>
2015-12-10 18:45:38 -0700

    added knight movement

--- test/pieces/knight-test.js
+++ test/pieces/knight-test.js
@@ -10,22 +10,42 @@ describe('Knight', function () {
 
   beforeEach (function () {
     this.board = new Board ();
-    this.square = new Square (this.board, 1, 6);
+    this.board.addSquaresToBoard();
+    this.square = this.board.findSquare(3, 4)
+    this.knight = new Knight (this.square, "black");
+    this.square.piece = this.knight;
   });
 
   it('should instantiate a new piece', function () {
-    let knight = new Knight ();
-    assert.isObject(knight);
+    assert.isObject(this.knight);
   });
 
   it('should know its square', function () {
-    let knight = new Knight (this.square, "black");
-    assert.equal(knight.square, this.square);
+    assert.equal(this.knight.square, this.square);
   });
 
   it('should know its color', function () {
-    let knight = new Knight (this.square, "black");
-    assert.equal(knight.color, "black");
+    assert.equal(this.knight.color, "black");
+  });
+
+  it('should know if it can move like a knight given empty spaces', function () {
+    let squareTwo = this.board.findSquare (1, 3);
+    let squareThree = this.board.findSquare (2, 2);
+    let squareFour = this.board.findSquare (2, 6);
+    let squareFive = this.board.findSquare (1, 5);
+    let squareSix = this.board.findSquare (4, 6);
+    let squareSeven = this.board.findSquare (4, 2);
+    let squareEight = this.board.findSquare (5, 3);
+    let squareNine = this.board.findSquare (5, 5);
+
+    assert(this.knight.canMoveTo(squareTwo));
+    assert(this.knight.canMoveTo(squareThree));
+    assert(this.knight.canMoveTo(squareFour));
+    assert(this.knight.canMoveTo(squareFive));
+    assert(this.knight.canMoveTo(squareSix));
+    assert(this.knight.canMoveTo(squareSeven));
+    assert(this.knight.canMoveTo(squareEight));
+    assert(this.knight.canMoveTo(squareNine));
   });
 
 });
