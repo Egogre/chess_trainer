56068c91421235555dda6437f38a1fd61f1959c1
George Hudson <georgehudson78@gmail.com>
2015-12-10 17:33:35 -0700

    bishop can move in diagonal directions only, set these vectors as attributes.

--- test/pieces/bishop-test.js
+++ test/pieces/bishop-test.js
@@ -10,22 +10,50 @@ describe('Bishop', function () {
 
   beforeEach (function () {
     this.board = new Board ();
-    this.square = new Square (this.board, 1, 6);
+    this.board.addSquaresToBoard();
+    this.square = this.board.findSquare(1, 6)
+    this.bishop = new Bishop (this.square, "black");
+    this.square.piece = this.bishop;
   });
 
   it('should instantiate a new piece', function () {
-    let bishop = new Bishop ();
-    assert.isObject(bishop);
+    assert.isObject(this.bishop);
   });
 
   it('should know its square', function () {
-    let bishop = new Bishop (this.square, "black");
-    assert.equal(bishop.square, this.square);
+    assert.equal(this.bishop.square, this.square);
   });
 
   it('should know its color', function () {
-    let bishop = new Bishop (this.square, "black");
-    assert.equal(bishop.color, "black");
+    assert.equal(this.bishop.color, "black");
+  });
+
+  it('should know if it can move up to any given empty spaces', function () {
+    let squareTwo = this.board.findSquare (2, 5);
+    let squareThree = this.board.findSquare (3, 4);
+    let squareFour = this.board.findSquare (4, 3);
+    let squareFive = this.board.findSquare (5, 2);
+    let squareSix = this.board.findSquare (6, 1);
+    let squareSeven = this.board.findSquare (7, 0);
+
+    assert(this.bishop.canMoveTo(squareTwo));
+    assert(this.bishop.canMoveTo(squareThree));
+    assert(this.bishop.canMoveTo(squareFour));
+    assert(this.bishop.canMoveTo(squareFive));
+    assert(this.bishop.canMoveTo(squareSix));
+    assert(this.bishop.canMoveTo(squareSeven));
+  });
+
+  it('should move north-east, south-west, north-west, south-east given empty spaces', function () {
+    let squareTwo = this.board.findSquare (0, 5);
+    let squareThree = this.board.findSquare (0, 7);
+    let squareFour = this.board.findSquare (2, 5);
+    let squareFive = this.board.findSquare (2, 7);
+
+    assert(this.bishop.canMoveTo(squareTwo));
+    assert(this.bishop.canMoveTo(squareThree));
+    assert(this.bishop.canMoveTo(squareFour));
+    assert(this.bishop.canMoveTo(squareFive));
   });
 
 });
