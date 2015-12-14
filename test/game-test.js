44b81d530be03f2040ddf3ecd548df385da92235
philmphoenix <jpholzmann@gmail.com>
2015-12-13 18:10:25 -0700

    Adds a test for whether the king is in check.

--- test/game-test.js
+++ test/game-test.js
@@ -5,12 +5,18 @@ var Game = require('../lib/game')
 var Board = require('../lib/board')
 var Square = require('../lib/square')
 var Piece = require('../lib/piece')
+var King = require('../lib/pieces/king')
+var Bishop = require('../lib/pieces/bishop')
 
 describe('Game', function () {
   beforeEach( function () {
     this.board = new Board ();
-    this.game = new Game (this.game);
+    this.game = new Game (this.board);
     this.board.game = this.game;
+    this.board.addSquaresToBoard ();
+    this.kingsSquare = this.board.findSquare(3, 4);
+    this.king = new King (this.kingsSquare, "black");
+    this.kingsSquare.piece = this.king;
   });
 
   it('should instantiate a new game', function () {
@@ -25,6 +31,17 @@ describe('Game', function () {
     this.game.toggleTurn();
 
     assert.equal(this.game.turn, "black");
+  });
+
+  it('should know if the king is in check', function () {
+    let squareTwo = this.board.findSquare (3, 2);
+    let squareThree = this.board.findSquare (2, 3)
+    let bishop = new Bishop (squareTwo, "white");
+    squareTwo.piece = bishop;
+
+    bishop.move(squareThree);
+
+    assert.equal(this.game.inCheck(), true);
   });
 
 });
