d029ce0ebf978a12152a20e83a8d47f487ed4020
George Hudson <georgehudson78@gmail.com>
2015-12-14 17:16:00 -0700

    a piece cannot move if that move would put own king in check.

--- test/piece-test.js
+++ test/piece-test.js
@@ -1,8 +1,13 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+var Game = require('../lib/game')
+var Board = require('../lib/board')
 var Square = require('../lib/square')
 var Piece = require('../lib/piece')
+var King = require('../lib/pieces/king')
+var Bishop = require('../lib/pieces/bishop')
+var Pawn = require('../lib/pieces/pawn')
 
 describe('Piece', function () {
 
@@ -28,6 +33,26 @@ describe('Piece', function () {
   it('should start its move counter at 0', function () {
     let piece = new Piece ();
     assert.equal(piece.moveCount, 0);
+  });
+
+  it('should cannot move if result puts own king in check', function () {
+    var board = new Board ();
+    board.addSquaresToBoard();
+    var game = new Game (board);
+    board.game = game;
+    var square = board.findSquare(1, 6);
+    var king = new King (square, "white");
+    square.piece = king;
+    var squareTwo = board.findSquare(4,3);
+    var squareThree = board.findSquare(3,4);
+    var squareFour = board.findSquare(3,3);
+    let bishop = new Bishop (squareTwo, "black");
+    squareTwo.piece = bishop;
+    let pawn = new Pawn (squareThree, "white");
+    squareThree.piece = pawn;
+
+    assert(pawn.canMoveTo(squareTwo));
+    assert(!pawn.canMoveTo(squareFour));
   });
 
 });
