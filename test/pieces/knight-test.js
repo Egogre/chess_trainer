b778f0a7ec32f9b2dce773171b48f4a9e71ff5df
George Hudson <georgehudson78@gmail.com>
2015-12-14 17:16:49 -0700

    implemented knight check for not putting own king in check

--- test/pieces/knight-test.js
+++ test/pieces/knight-test.js
@@ -1,19 +1,29 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+var Game = require('../../lib/game')
 var Board = require('../../lib/board')
 var Square = require('../../lib/square')
 var Piece = require('../../lib/piece')
 var Knight = require('../../lib/pieces/knight')
+var King = require('../../lib/pieces/king')
 
 describe('Knight', function () {
 
   beforeEach (function () {
     this.board = new Board ();
+    this.game = new Game (this.board);
+    this.board.game = this.game;
     this.board.addSquaresToBoard();
     this.square = this.board.findSquare(3, 4);
     this.knight = new Knight (this.square, "black");
     this.square.piece = this.knight;
+    this.whiteKingsSquare = this.board.findSquare(4, 7);
+    this.whiteKing = new King (this.whiteKingsSquare, "white");
+    this.whiteKingsSquare.piece = this.whiteKing;
+    this.blackKingsSquare = this.board.findSquare(4, 0);
+    this.blackKing = new King (this.blackKingsSquare, "black");
+    this.blackKingsSquare.piece = this.blackKing;
   });
 
   it('should instantiate a new piece', function () {
@@ -29,6 +39,7 @@ describe('Knight', function () {
   });
 
   it('should know if it can move like a knight given empty spaces', function () {
+    this.game.turn = "black";
     let squareTwo = this.board.findSquare (1, 3);
     let squareThree = this.board.findSquare (2, 2);
     let squareFour = this.board.findSquare (2, 6);
